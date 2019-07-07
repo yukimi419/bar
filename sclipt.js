3425509b45864184a2ef4aaa599679c18cdfe07c
yukimi419 <52624554+yukimi419@users.noreply.github.com>
2019-07-07 20:02:50 +0900

    Add files via upload

---
+++ sclipt.js
@@ -0,0 +1,17 @@
+$(function(){
+
+  $('#top-btn').click(function(){
+    $('html,body').animate({
+      'scrollTop': 0
+    },800);
+  });
+
+  $('header a').click(function(){
+    var id = $(this).attr('href');
+    var position = $(id).offset().top;
+    $('html,body').animate({
+      'scrollTop': position - 100
+    },500);
+  });
+
+});
